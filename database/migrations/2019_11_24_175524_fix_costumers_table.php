fa13c66235602df08b59ce41dfdc488395181f57
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-24 19:00:44 +0100

    fixed costumers

---
+++ database/migrations/2019_11_24_175524_fix_costumers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixCostumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('email');
+            $table->dropColumn('phone');
+            $table->string('phone')->unique()->default("No_Phone");
+            $table->string('email')->unique()->default("No_Email");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('email');
+            $table->dropColumn('phone');
+            $table->string('phone')->nullable();
+            $table->string('email')->unique();
+        });
+    }
+}
