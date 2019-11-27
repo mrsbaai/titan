3fcbe622d66d7b4eef94d32508dc6e478f671919
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-27 21:57:36 +0100

    Update thankyou.blade.php

--- resources/views/titan/thankyou.blade.php
+++ resources/views/titan/thankyou.blade.php
@@ -457,18 +457,18 @@
         <h2 class="success-page__title" style="font-size:300%">!شكرا لطلبك هذا المنتج</h2>
 
         
-            <p class="success-page__message_success" style="font-size:300%">.سنقوم بالرد على طلبك قريبًا، المرجو الاحتفاظ بهاتفك على وضع التشغيل</p>
+            <p class="success-page__message_success" style="font-size:200%">.سنقوم بالرد على طلبك قريبًا، المرجو الاحتفاظ بهاتفك على وضع التشغيل</p>
 
             <h3 class="success-page__text" style="font-size:300%">:يرجى التحقق من معلومات الاتصال الخاصة بك</h3>
 
             <div class="list-info">
                 <ul class="list-info__list">
-                    <li class="list-info__item" style="font-size:300%">
+                    <li class="list-info__item" style="font-size:200%">
                         <span class="list-info__text">{{$name}}</span>
                         :الاسم
                         
                     </li>
-                    <li class="list-info__item" style="font-size:300%">
+                    <li class="list-info__item" style="font-size:200%">
                         <span class="list-info__text">{{$phone}}</span>
                         :رقم الهاتف
                        
