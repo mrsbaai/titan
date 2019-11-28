fdac0517ea7db7ca4cd55ae13cf7f7e57f357638
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-28 23:54:04 +0100

    Update DropdownController.php

--- app/Http/Controllers/DropdownController.php
+++ app/Http/Controllers/DropdownController.php
@@ -21,9 +21,10 @@ class DropdownController extends Controller
 
         public function getPosteList(Request $request)
         {
-            $postes = DB::table("poste")
+
+            $postes = DB::table("poste")->select(DB::raw('CONCAT(NOM_AGENCE, " - ", ADRESSE) AS addr'))
             ->where("VILLECOMMUNE",$request->city_id)
-            ->pluck("ADRESSE","ADRESSE");
+            ->pluck("addr","addr");
             return response()->json($postes);
         }
 }
