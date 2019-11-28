b58eb8861421723779426314cef2540f294d312a
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-28 21:41:58 +0100

    Update landing.blade.php

--- resources/views/titan/landing.blade.php
+++ resources/views/titan/landing.blade.php
@@ -380,8 +380,7 @@
 <div class="footer__inner">
 <div class="footer__title">
 <div class="footer__title-top">
-<div class="footer__title-top-left">امنحها</div>
-<div class="footer__title-top-left">الشعور والإثارة</div>
+<div class="footer__title-top-left">امنحها الشعور والإثارة</div>
 
 </div>
 <div class="footer__title-bottom">التي تستحقها</div>
