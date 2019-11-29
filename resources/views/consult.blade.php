ce76daa966f833343ee37e8fe9393ce2f49a6d10
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-29 21:48:50 +0100

    Update consult.blade.php

--- resources/views/consult.blade.php
+++ resources/views/consult.blade.php
@@ -91,10 +91,7 @@
                 </div>
 
                 <div class="form-row">
-                    <div class="form-group col-md-6 ">
-                    @if($PreviousID)<a href="{{route('consult', ['id' => $PreviousID, 'status' => $ret->status])}}"><button class="btn btn-lg btn-gray">< Previous</button></a>@endif
-                    @if($NextID)<a href="{{route('consult', ['id' => $NextID, 'status' => $ret->status])}}"><button class="btn btn-lg btn-gray">Next ></button></a>@endif
-                    </div>
+
 
 
                     <div class="form-group col-md-6 text-right">
@@ -108,7 +105,10 @@
                 </div>
 
 
-                
+                <div class="form-group col-md-6 ">
+                        @if($PreviousID)<a href="{{route('consult', ['id' => $PreviousID, 'status' => $ret->status])}}"><button class="btn btn-lg btn-gray">< Previous</button></a>@endif
+                        @if($NextID)<a href="{{route('consult', ['id' => $NextID, 'status' => $ret->status])}}"><button class="btn btn-lg btn-gray">Next ></button></a>@endif
+                        </div>
 
                 {{ Form::close() }}
 
