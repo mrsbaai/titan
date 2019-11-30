20692043c3867b95094f14c8641e69fb02623218
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-30 14:46:39 +0100

    Update consult.blade.php

--- resources/views/consult.blade.php
+++ resources/views/consult.blade.php
@@ -13,10 +13,10 @@
                 {{ Form::open(array('action' => 'HomeController@updateCostumer', 'id' => 'update'))}}
                 <input type="hidden" name="idOrder" id="idOrder" value="{{$ret->id}}" >
                 <div class="form-row">
-                <div class="form-group col-md-12" style="font-size:120%;">Created: <span id="created">{{$ret->created_at}}</span><div  style="float:right;" id="created2"></div></div>
-                <div class="form-group col-md-12" style="font-size:120%;">Updated: <span id="updated">{{$ret->updated_at}}</span><div  style="float:right;" id="updated2"></div></div>
+                <div class="form-group col-md-12" style="font-size:150%;">Created: <span id="created">{{$ret->created_at}}</span><div  style="float:right;" id="created2"></div></div>
+                <div class="form-group col-md-12" style="font-size:150%;">Updated: <span id="updated">{{$ret->updated_at}}</span><div  style="float:right;" id="updated2"></div></div>
                 </div>
-                
+                <hr/>
 
                 <div class="form-row">
 
@@ -117,14 +117,14 @@
                         <textarea class="form-control" id="inputNotes" name="inputNotes" rows="6">{{$ret->notes}}</textarea>
                 </div>
 
-                <div class="form-row">
-                        <div class="form-group">
+          
+                <hr/>
                                 <div class="form-group">
-                                    <label for="inputStatus">Change Status</label>
+                                    <label for="inputStatus">Update Status</label>
                                     {!! Form::select('inputStatus', $allStatus, $ret->status, ['class' => 'form-control']) !!}
                                 </div>
-                        </div>
-                    </div>
+                     
+                  
     
                       
 
