9c918f13095e7163bc21d5ff9a282387cf7c72d9
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-30 16:30:42 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'Africa/Morocco',
+    'timezone' => 'Africa/Casablanca',
 
     /*
     |--------------------------------------------------------------------------
