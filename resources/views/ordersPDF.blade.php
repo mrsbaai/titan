6c18f11af7f3fc7276ba80c24d11f5e35069a61f
mrsbaai <abdelilah.sbaai@gmail.com>
2019-11-30 15:29:11 +0100

    up

--- resources/views/ordersPDF.blade.php
+++ resources/views/ordersPDF.blade.php
@@ -1,73 +1,50 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Processed Orders</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-                <h1>{{ $title }}</h1>
-                <p>some content</p>
-        </div>
-    </body>
-</html>
+<br/>Order#: 2 - Titan Gel - Processed</b>
+<br/>
+<br/>Created: 2019-11-29 21:06:14
+<br/>Updated: 2019-11-30 12:57:21
+<br/>
+<br/>Amount: 1
+<br/>Unit Price (Dirhams): 449
+<br/>Shipping Price (Dirhams): 449
+<br/>Total Price (Dirhams): 449
+<br/>
+<br/>Full Name: hamid kamal
+<br/>Pone Number: 0645678905
+<br/>Age: 30
+<br/>
+<br/>Region: 
+<br/>City: 
+<br/>Poste Address: 
+<br/>Home Address: 
+<br/>Shipping Type: 
+<br/>Tracking Code: 
+<br/>
+<br/>Notes: 
+<br/>
+<hr/>
+<br/>
+<br/>Order#: 2 - Titan Gel - Processed</b>
+<br/>
+<br/>Created: 2019-11-29 21:06:14
+<br/>Updated: 2019-11-30 12:57:21
+<br/>
+<br/>Amount: 1
+<br/>Unit Price (Dirhams): 449
+<br/>Shipping Price (Dirhams): 449
+<br/>Total Price (Dirhams): 449
+<br/>
+<br/>Full Name: hamid kamal
+<br/>Pone Number: 0645678905
+<br/>Age: 30
+<br/>
+<br/>Region: 
+<br/>City: 
+<br/>Poste Address: 
+<br/>Home Address: 
+<br/>Shipping Type: 
+<br/>Tracking Code: 
+<br/>
+<br/>Notes: 
+<br/>
+<hr/>
+<br/>
